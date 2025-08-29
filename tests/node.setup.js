e259eab4666555e3550a668465d75766e8fe8ffb
Takuya Matsuyama <hi@craftz.dog>
2025-08-29 14:18:31 +0900

    fix(tests): sqlite3 adapter not loading properly

--- tests/node.setup.js
+++ tests/node.setup.js
@@ -2,11 +2,14 @@ const chai = (global.chai = require('chai'))
 chaiAsPromised = require('chai-as-promised')
 global.should = chai.should()
 global.assert = chai.assert
-const PouchDB = (global.PouchDB = require('pouchdb'))
+const PouchDB = require('pouchdb')
 global.testUtils = require('./utils')
 
 chai.use(chaiAsPromised.default)
 
 // Register the SQLite3 adapter
-const SQLite3Adapter = require('../lib/index')
+const SQLite3Adapter = require('../lib/index').default
 PouchDB.plugin(SQLite3Adapter)
+global.PouchDB = PouchDB.defaults({
+  adapter: 'sqlite3'
+})
