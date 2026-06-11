inkdropapp/pouchdb-adapter-node-sqlite

commit 631d40d8c3371b79324ed155b25833e3ce81e190

chore(*): start over

src/utils.ts
@@ -1,74 +1,165 @@
-import { safeJsonStringify, safeJsonParse } from 'pouchdb-json'
+import { createError, WSQ_ERROR } from 'pouchdb-errors'
+import { guardedConsole } from 'pouchdb-utils'
+import { BY_SEQ_STORE, ATTACH_STORE, ATTACH_AND_SEQ_STORE } from './constants'
+import type { Transaction } from '@op-engineering/op-sqlite'
 
-export function qMarks(num: number): string {
-  let accum = '?'
-  for (let i = 1; i < num; i++) {
-    accum += ', ?'
-  }
-  return accum
-}
-
-export function stringifyDoc(doc: any): string {
+function stringifyDoc(doc: Record<string, any>): string {
+  // don't bother storing the id/rev. it uses lots of space,
+  // in persistent map/reduce especially
   delete doc._id
   delete doc._rev
-  return safeJsonStringify(doc)
+  return JSON.stringify(doc)
 }
 
-export function unstringifyDoc(doc: string, id: string, rev: string): any {
-  const parsed = safeJsonParse(doc)
-  parsed._id = id
-  parsed._rev = rev
-  return parsed
+function unstringifyDoc(
+  doc: string,
+  id: string,
+  rev: string
+): Record<string, any> {
+  const parsedDoc = JSON.parse(doc)
+  parsedDoc._id = id
+  parsedDoc._rev = rev
+  return parsedDoc
 }
 
-export function select(
-  selectStmt: string,
-  from: string | string[],
-  joiner?: string,
+// question mark groups IN queries, e.g. 3 -> '(?,?,?)'
+function qMarks(num: number): string {
+  let s = '('
+  while (num--) {
+    s += '?'
+    if (num) {
+      s += ','
+    }
+  }
+  return s + ')'
+}
+
+function select(
+  selector: string,
+  table: string | string[],
+  joiner?: string | null,
   where?: string | string[],
   orderBy?: string
 ): string {
   return (
     'SELECT ' +
-    selectStmt +
+    selector +
     ' FROM ' +
-    (typeof from === 'string' ? from : from.join(' JOIN ')) +
+    (typeof table === 'string' ? table : table.join(' JOIN ')) +
     (joiner ? ' ON ' + joiner : '') +
     (where
-      ? ' WHERE ' +
-        (typeof where === 'string' ? where : where.join(' AND '))
+      ? ' WHERE ' + (typeof where === 'string' ? where : where.join(' AND '))
       : '') +
     (orderBy ? ' ORDER BY ' + orderBy : '')
   )
 }
 
-export function compactRevs(revs: string[], docId: string, db: any): void {
-  const pairs: Array<[string, string]> = []
-  revs.forEach(function (rev) {
-    const idx = rev.indexOf('-')
-    const prefix = rev.substring(0, idx)
-    const suffix = rev.substring(idx + 1)
-    pairs.push([prefix, suffix])
-  })
-
-  const sqlArgs: any[] = []
-  pairs.forEach(function (pair) {
-    sqlArgs.push(docId)
-    sqlArgs.push(pair[0])
-    sqlArgs.push(pair[1])
-  })
-
-  const sql =
-    'DELETE FROM "by-sequence" WHERE doc_id=? AND rev IN (' +
-    pairs.map(() => '(? || "-" || ?)').join(', ') +
-    ')'
-
-  db.run(sql, ...sqlArgs)
-}
-
-export function handleSQLiteError(err: any, callback?: Function) {
-  if (callback) {
-    return callback(err)
+async function compactRevs(
+  revs: string[],
+  docId: string,
+  tx: Transaction
+): Promise<void> {
+  if (!revs.length) {
+    return
   }
-  throw err
+
+  let numDone = 0
+  const seqs: number[] = []
+
+  function checkDone() {
+    if (++numDone === revs.length) {
+      // done
+      deleteOrphans()
+    }
+  }
+
+  async function deleteOrphans() {
+    // find orphaned attachment digests
+
+    if (!seqs.length) {
+      return
+    }
+
+    let sql =
+      'SELECT DISTINCT digest AS digest FROM ' +
+      ATTACH_AND_SEQ_STORE +
+      ' WHERE seq IN ' +
+      qMarks(seqs.length)
+
+    let res = await tx.execute(sql, seqs)
+    const digestsToCheck: string[] = []
+    if (res.rows) {
+      for (let i = 0; i < res.rows.length; i++) {
+        digestsToCheck.push(res.rows[i]!.digest as string)
+      }
+    }
+    if (!digestsToCheck.length) {
+      return
+    }
+
+    sql =
+      'DELETE FROM ' +
+      ATTACH_AND_SEQ_STORE +
+      ' WHERE seq IN (' +
+      seqs.map(() => '?').join(',') +
+      ')'
+    await tx.execute(sql, seqs)
+    sql =
+      'SELECT digest FROM ' +
+      ATTACH_AND_SEQ_STORE +
+      ' WHERE digest IN (' +
+      digestsToCheck.map(() => '?').join(',') +
+      ')'
+    res = await tx.execute(sql, digestsToCheck)
+    const nonOrphanedDigests = new Set<string>()
+    if (res.rows) {
+      for (let i = 0; i < res.rows.length; i++) {
+        nonOrphanedDigests.add(res.rows[i]!.digest as string)
+      }
+    }
+    for (const digest of digestsToCheck) {
+      if (nonOrphanedDigests.has(digest)) {
+        return
+      }
+      await tx.execute(
+        'DELETE FROM ' + ATTACH_AND_SEQ_STORE + ' WHERE digest=?',
+        [digest]
+      )
+      await tx.execute('DELETE FROM ' + ATTACH_STORE + ' WHERE digest=?', [
+        digest,
+      ])
+    }
+  }
+
+  // update by-seq and attach stores in parallel
+  for (const rev of revs) {
+    const sql = 'SELECT seq FROM ' + BY_SEQ_STORE + ' WHERE doc_id=? AND rev=?'
+
+    const res = await tx.execute(sql, [docId, rev])
+    if (!res.rows?.length) {
+      // already deleted
+      return checkDone()
+    }
+    const seq = res.rows[0]!.seq as number
+    seqs.push(seq)
+
+    await tx.execute('DELETE FROM ' + BY_SEQ_STORE + ' WHERE seq=?', [seq])
+  }
 }
+
+export function handleSQLiteError(
+  event: Error,
+  callback?: (error: any) => void
+) {
+  guardedConsole('error', 'SQLite threw an error', event)
+  // event may actually be a SQLError object, so report is as such
+  const errorNameMatch =
+    event && event.constructor.toString().match(/function ([^(]+)/)
+  const errorName = (errorNameMatch && errorNameMatch[1]) || event.name
+  const errorReason = event.message
+  const error = createError(WSQ_ERROR, errorReason, errorName)
+  if (callback) callback(error)
+  else return error
+}
+
+export { stringifyDoc, unstringifyDoc, qMarks, select, compactRevs }

src/bulkDocs.ts
@@ -1,306 +1,327 @@
-import { uuid } from 'pouchdb-utils'
-import crypto from 'crypto'
+import {
+  preprocessAttachments,
+  isLocalId,
+  processDocs,
+  parseDoc,
+} from 'pouchdb-adapter-utils'
+import { compactTree } from 'pouchdb-merge'
 import { safeJsonParse, safeJsonStringify } from 'pouchdb-json'
-import { createError } from 'pouchdb-errors'
-import { isLocalId } from 'pouchdb-merge'
+import { MISSING_STUB, createError } from 'pouchdb-errors'
 
 import {
   DOC_STORE,
   BY_SEQ_STORE,
-  LOCAL_STORE,
   ATTACH_STORE,
-  ATTACH_AND_SEQ_STORE
+  ATTACH_AND_SEQ_STORE,
 } from './constants'
 
-import { stringifyDoc } from './utils'
-import type Database from 'better-sqlite3'
+import { select, stringifyDoc, compactRevs, handleSQLiteError } from './utils'
+import type { Transaction } from '@op-engineering/op-sqlite'
+import { logger } from './debug'
 
-function insertDoc(
-  docInfo: any,
-  winningRev: string,
-  winningRevIsDeleted: boolean,
-  newRevIsDeleted: boolean,
-  isUpdate: boolean,
-  delta: number,
-  resultsIdx: number,
-  callback: Function,
-  db: Database.Database
-) {
-  const doc = docInfo.data
-  const id = doc._id
-  const oldRev = doc._rev
-  const docId = isLocalId(id) ? null : id
-  // const newSeq = isUpdate ? docInfo.winningSeq : null
-
-  // Calculate new revision first
-  let newRev: string
-  if (docInfo.metadata.rev) {
-    const revParts = docInfo.metadata.rev.split('-')
-    const revNum = parseInt(revParts[0]) + 1
-    const hash = uuid().replace(/-/g, '').substring(0, 32)
-    newRev = revNum + '-' + hash
-  } else {
-    const hash = uuid().replace(/-/g, '').substring(0, 32)
-    newRev = '1-' + hash
-  }
-  
-  // Process attachments first to get digests
-  const attachments = Object.keys(doc._attachments || {})
-  const docToStore = Object.assign({}, doc)
-  
-  if (attachments.length > 0) {
-    docToStore._attachments = {}
-    attachments.forEach(key => {
-      const att = doc._attachments[key]
-      if (!att.stub && att.data) {
-        // Calculate digest for new attachments
-        const md5 = crypto.createHash('md5')
-        md5.update(Buffer.from(att.data, 'base64'))
-        att.digest = 'md5-' + md5.digest('hex')
-        att.length = Buffer.from(att.data, 'base64').length
-        att.revpos = parseInt(newRev.split('-')[0])
-      }
-      // Store metadata only in document
-      docToStore._attachments[key] = {
-        content_type: att.content_type,
-        digest: att.digest,
-        length: att.length,
-        revpos: att.revpos,
-        stub: true
-      }
-    })
-  }
-  
-  const json = stringifyDoc(docToStore)
-
-  let insertSql: string
-  if (newRevIsDeleted) {
-    insertSql = 'INSERT INTO ' + BY_SEQ_STORE + 
-      ' (doc_id, rev, json, deleted) VALUES (?, ?, ?, 1);'
-  } else {
-    insertSql = 'INSERT INTO ' + BY_SEQ_STORE + 
-      ' (doc_id, rev, json, deleted) VALUES (?, ?, ?, 0);'
-  }
-
-  const insertStmt = db.prepare(insertSql)
-  try {
-    const result = insertStmt.run(docId, newRev || oldRev, json)
-    const seq = result.lastInsertRowid as number
-
-    if (isLocalId(id)) {
-      const localSql = 'INSERT OR REPLACE INTO ' + LOCAL_STORE + 
-        ' (id, rev, json) VALUES (?, ?, ?);'
-      db.prepare(localSql).run(id, newRev || oldRev, json)
-      resultsIdx++
-      callback()
-      return
-    }
-
-    // Update metadata with new revision
-    docInfo.metadata.rev = newRev
-    docInfo.metadata.seq = seq
-
-    const metadataToStore = {
-      id: id,
-      rev_tree: [{pos: 1, ids: [newRev.split('-')[1], {}, []]}],
-      rev: newRev,
-      deleted: newRevIsDeleted ? 1 : 0,
-      seq: seq
-    }
-
-    const metadataStr = safeJsonStringify(metadataToStore)
-    let sql: string
-
-    if (isUpdate) {
-      sql = 'UPDATE ' + DOC_STORE + ' SET json=?, max_seq=?, rev=?, winningseq=? WHERE id=?'
-      db.prepare(sql).run(metadataStr, seq, newRev, seq, id)
-    } else {
-      sql = 'INSERT INTO ' + DOC_STORE + ' (id, winningseq, max_seq, json, rev) VALUES (?, ?, ?, ?, ?)'
-      db.prepare(sql).run(id, seq, seq, metadataStr, newRev)
-    }
-
-    docInfo.metadata.seq = seq
-
-    // Save attachments if any
-    if (attachments.length === 0) {
-      finish()
-    } else {
-      let attachmentsSaved = 0
-      const totalAttachments = attachments.length
-      
-      const checkDone = () => {
-        attachmentsSaved++
-        if (attachmentsSaved === totalAttachments) {
-          finish()
-        }
-      }
-      
-      attachments.forEach(key => {
-        const attachment = doc._attachments[key]
-        if (!attachment.stub && attachment.data) {
-          const data = attachment.data
-          saveAttachment(attachment, key, newRev, seq, data, checkDone, db)
-        } else {
-          checkDone()
-        }
-      })
-    }
-
-    function finish() {
-      resultsIdx++
-      callback()
-    }
-  } catch (err) {
-    callback(err)
-  }
+interface DocInfo {
+  _id: string
+  metadata: any
+  data: any
+  stemmedRevs?: string[]
+  error?: any
 }
 
-function saveAttachment(
-  attachment: any, 
-  key: string, 
-  rev: string, 
-  seq: number, 
-  data: string, 
-  callback: Function,
-  db: Database.Database
-) {
-  const digest = attachment.digest
-  const escaped = 0 // Not escaped for now
-  
-  try {
-    // Store attachment data
-    const insertAttachSql = 'INSERT OR IGNORE INTO ' + ATTACH_STORE + ' (digest, escaped, body) VALUES (?, ?, ?)'
-    db.prepare(insertAttachSql).run(digest, escaped, data)
-    
-    // Link attachment to document sequence
-    const insertAttachSeqSql = 'INSERT INTO ' + ATTACH_AND_SEQ_STORE + ' (digest, seq) VALUES (?, ?)'
-    db.prepare(insertAttachSeqSql).run(digest, seq)
-    
-    callback()
-  } catch (err) {
-    callback(err)
-  }
+interface DBOptions {
+  revs_limit?: number
 }
 
-export default function(
-  dbOpts: any,
-  req: any,
-  opts: any,
+interface Request {
+  docs: any[]
+}
+
+interface Options {
+  new_edits: boolean
+}
+
+async function sqliteBulkDocs(
+  dbOpts: DBOptions,
+  req: Request,
+  opts: Options,
   api: any,
-  db: Database.Database,
-  transaction: Function,
-  Changes: any,
-  callback: Function
-) {
-  // const newEdits = opts.new_edits !== false
+  transaction: (fn: (tx: Transaction) => Promise<void>) => Promise<void>,
+  sqliteChanges: any
+): Promise<any> {
+  const newEdits = opts.new_edits
   const userDocs = req.docs
-  const docsToInsert = userDocs.length
-  const docInfos: any[] = new Array(docsToInsert)
-  const allDocInfos: any[] = [] // Keep a copy for completion
-  const docInfoErrors: any[] = []
-  const resultsIdx = 0
 
-  userDocs.forEach(function(doc: any, i: number) {
-    if (!doc._id) {
-      doc._id = uuid()
+  const docInfos: DocInfo[] = userDocs.map((doc) => {
+    if (doc._id && isLocalId(doc._id)) {
+      return doc
     }
-    const docInfo = {
-      data: doc,
-      metadata: {
-        id: doc._id,
-        rev: null,
-        rev_tree: null
-      }
-    }
-    docInfos[i] = docInfo
-    allDocInfos[i] = docInfo
+    return parseDoc(doc, newEdits, dbOpts)
   })
 
-  transaction(function() {
-    const fetchExistingSql = 'SELECT json FROM ' + DOC_STORE + ' WHERE id = ?'
-    const fetchExistingStmt = db.prepare(fetchExistingSql)
+  const docInfoErrors = docInfos.filter((docInfo) => docInfo.error)
+  if (docInfoErrors.length) {
+    throw docInfoErrors[0]
+  }
 
-    docInfos.forEach(function(docInfo: any) {
-      const id = docInfo.data._id
-      const isLocal = isLocalId(id)
+  let tx: Transaction
+  const results = new Array(docInfos.length)
+  const fetchedDocs = new Map<string, any>()
 
-      if (isLocal) {
-        const existingDoc = db.prepare('SELECT rev, json FROM ' + LOCAL_STORE + ' WHERE id = ?').get(id)
-        
-        if (existingDoc) {
-          const doc = existingDoc as any
-          docInfo.metadata.rev = doc.rev
-          docInfo.metadata.rev_tree = [{
-            pos: parseInt(doc.rev.split('-')[0], 10),
-            ids: [doc.rev.split('-')[1], {}, []]
-          }]
-        } else {
-          docInfo.metadata.rev = '0-1'
-          docInfo.metadata.rev_tree = [{
-            pos: 1,
-            ids: ['1', {}, []]
-          }]
-        }
-      } else {
-        const existingDoc = fetchExistingStmt.get(id)
-        
-        if (existingDoc) {
-          const doc = existingDoc as any
-          const metadata = safeJsonParse(doc.json)
-          docInfo.metadata = metadata
-          // Check if the incoming document has the correct revision
-          if (docInfo.data._rev && docInfo.data._rev !== metadata.rev) {
-            docInfoErrors[docInfos.indexOf(docInfo)] = createError('REV_CONFLICT')
+  async function verifyAttachment(digest: string) {
+    logger.debug('verify attachment:', digest)
+    const sql =
+      'SELECT count(*) as cnt FROM ' + ATTACH_STORE + ' WHERE digest=?'
+    const result = await tx.execute(sql, [digest])
+    if (result.rows[0]?.cnt === 0) {
+      const err = createError(
+        MISSING_STUB,
+        'unknown stub attachment with digest ' + digest
+      )
+      logger.error('unknown:', err)
+      throw err
+    } else {
+      logger.debug('ok')
+      return true
+    }
+  }
+
+  async function verifyAttachments(): Promise<void> {
+    const digests: string[] = []
+    docInfos.forEach((docInfo) => {
+      if (docInfo.data && docInfo.data._attachments) {
+        Object.keys(docInfo.data._attachments).forEach((filename) => {
+          const att = docInfo.data._attachments[filename]
+          if (att.stub) {
+            logger.debug('attachment digest', att.digest)
+            digests.push(att.digest)
           }
-        } else {
-          docInfo.metadata.rev = null
-          docInfo.metadata.rev_tree = []
-        }
+        })
       }
     })
 
-    processDocs()
+    if (!digests.length) return
 
-    function processDocs() {
-      if (!docInfos.length) {
-        return complete()
+    for (const digest of digests) {
+      await verifyAttachment(digest)
+    }
+  }
+
+  async function writeDoc(
+    docInfo: DocInfo,
+    winningRev: string,
+    _winningRevIsDeleted: boolean,
+    newRevIsDeleted: boolean,
+    isUpdate: boolean,
+    _delta: number,
+    resultsIdx: number
+  ) {
+    logger.debug('writeDoc:', { ...docInfo, data: null })
+
+    async function dataWritten(tx: Transaction, seq: number) {
+      const id = docInfo.metadata.id
+
+      let revsToCompact = docInfo.stemmedRevs || []
+      if (isUpdate && api.auto_compaction) {
+        revsToCompact = compactTree(docInfo.metadata).concat(revsToCompact)
       }
-      
-      const currentDoc = docInfos.shift()
-      const isUpdate = currentDoc.metadata.rev !== null
-      const isDeleted = currentDoc.data._deleted === true
-      insertDoc(
-        currentDoc,
-        currentDoc.metadata.rev,
-        false,
-        isDeleted,
-        isUpdate,
-        0,
-        resultsIdx,
-        function() {
-          processDocs()
-        },
-        db
-      )
+      if (revsToCompact.length) {
+        compactRevs(revsToCompact, id, tx)
+      }
+
+      docInfo.metadata.seq = seq
+      const rev = docInfo.metadata.rev
+      delete docInfo.metadata.rev
+
+      const sql = isUpdate
+        ? 'UPDATE ' +
+          DOC_STORE +
+          ' SET json=?, max_seq=?, winningseq=' +
+          '(SELECT seq FROM ' +
+          BY_SEQ_STORE +
+          ' WHERE doc_id=' +
+          DOC_STORE +
+          '.id AND rev=?) WHERE id=?'
+        : 'INSERT INTO ' +
+          DOC_STORE +
+          ' (id, winningseq, max_seq, json) VALUES (?,?,?,?);'
+      const metadataStr = safeJsonStringify(docInfo.metadata)
+      const params = isUpdate
+        ? [metadataStr, seq, winningRev, id]
+        : [id, seq, seq, metadataStr]
+      await tx.execute(sql, params)
+      results[resultsIdx] = {
+        ok: true,
+        id: docInfo.metadata.id,
+        rev: rev,
+      }
+      fetchedDocs.set(id, docInfo.metadata)
     }
 
-    function complete() {
-      const results: any[] = []
-      
-      allDocInfos.forEach(function(docInfo: any, idx: number) {
-        if (docInfoErrors[idx]) {
-          results.push(docInfoErrors[idx])
-        } else {
-          results.push({
-            ok: true,
-            id: docInfo.metadata.id,
-            rev: docInfo.metadata.rev
-          })
-        }
-      })
+    async function insertAttachmentMappings(seq: number) {
+      const attsToAdd = Object.keys(data._attachments || {})
 
-      Changes.notify(api._name)
-      callback(null, results)
+      if (!attsToAdd.length) {
+        return
+      }
+
+      function add(att: string) {
+        const sql =
+          'INSERT INTO ' + ATTACH_AND_SEQ_STORE + ' (digest, seq) VALUES (?,?)'
+        const sqlArgs = [data._attachments[att].digest, seq]
+        return tx.execute(sql, sqlArgs)
+      }
+
+      await Promise.all(attsToAdd.map((att) => add(att)))
+    }
+
+    docInfo.data._id = docInfo.metadata.id
+    docInfo.data._rev = docInfo.metadata.rev
+    const attachments = Object.keys(docInfo.data._attachments || {})
+
+    if (newRevIsDeleted) {
+      docInfo.data._deleted = true
+    }
+
+    for (const key of attachments) {
+      const att = docInfo.data._attachments[key]
+      if (!att.stub) {
+        const data = att.data
+        delete att.data
+        att.revpos = parseInt(winningRev, 10)
+        const digest = att.digest
+        await saveAttachment(digest, data)
+      }
+    }
+
+    const data = docInfo.data
+    const deletedInt = newRevIsDeleted ? 1 : 0
+
+    const id = data._id
+    const rev = data._rev
+    const json = stringifyDoc(data)
+    const sql =
+      'INSERT INTO ' +
+      BY_SEQ_STORE +
+      ' (doc_id, rev, json, deleted) VALUES (?, ?, ?, ?);'
+    const sqlArgs = [id, rev, json, deletedInt]
+
+    try {
+      const result = await tx.execute(sql, sqlArgs)
+      const seq = result.insertId
+      if (typeof seq === 'number') {
+        await insertAttachmentMappings(seq)
+        await dataWritten(tx, seq)
+      }
+    } catch (e) {
+      // constraint error, recover by updating instead (see #1638)
+      // https://github.com/pouchdb/pouchdb/issues/1638
+      const fetchSql = select('seq', BY_SEQ_STORE, null, 'doc_id=? AND rev=?')
+      const res = await tx.execute(fetchSql, [id, rev])
+      const seq = res.rows[0]!.seq as number
+      logger.debug(
+        `Got a constraint error, updating instead: seq=${seq}, id=${id}, rev=${rev}`
+      )
+      const sql =
+        'UPDATE ' +
+        BY_SEQ_STORE +
+        ' SET json=?, deleted=? WHERE doc_id=? AND rev=?;'
+      const sqlArgs = [json, deletedInt, id, rev]
+      await tx.execute(sql, sqlArgs)
+      await insertAttachmentMappings(seq)
+      await dataWritten(tx, seq)
+    }
+  }
+
+  function websqlProcessDocs(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      let chain = Promise.resolve()
+      processDocs(
+        dbOpts.revs_limit,
+        docInfos,
+        api,
+        fetchedDocs,
+        tx,
+        results,
+        (
+          docInfo: DocInfo,
+          winningRev: string,
+          winningRevIsDeleted: boolean,
+          newRevIsDeleted: boolean,
+          isUpdate: boolean,
+          delta: number,
+          resultsIdx: number,
+          callback: (err?: any) => void
+        ) => {
+          chain = chain.then(() => {
+            return writeDoc(
+              docInfo,
+              winningRev,
+              winningRevIsDeleted,
+              newRevIsDeleted,
+              isUpdate,
+              delta,
+              resultsIdx
+            ).then(callback, callback)
+          })
+        },
+        opts,
+        (err?: any) => {
+          if (!err) resolve()
+          else reject(err)
+        }
+      )
+    })
+  }
+
+  async function fetchExistingDocs(): Promise<void> {
+    if (!docInfos.length) return
+
+    for (const docInfo of docInfos) {
+      if (docInfo._id && isLocalId(docInfo._id)) {
+        continue
+      }
+      const id = docInfo.metadata.id
+      const result = await tx.execute(
+        'SELECT json FROM ' + DOC_STORE + ' WHERE id = ?',
+        [id]
+      )
+      if (result.rows?.length) {
+        const metadata = safeJsonParse(result.rows[0]!.json)
+        fetchedDocs.set(id, metadata)
+      }
+    }
+  }
+
+  async function saveAttachment(digest: string, data: any) {
+    logger.debug('saveAttachment:', digest)
+    let sql = 'SELECT digest FROM ' + ATTACH_STORE + ' WHERE digest=?'
+    const result = await tx.execute(sql, [digest])
+    if (result.rows?.length) return
+    sql =
+      'INSERT INTO ' + ATTACH_STORE + ' (digest, body, escaped) VALUES (?,?,0)'
+    await tx.execute(sql, [digest, data])
+  }
+
+  await new Promise<void>((resolve, reject) => {
+    preprocessAttachments(docInfos, 'binary', (err: any) => {
+      if (err) reject(err)
+      else resolve()
+    })
+  })
+
+  await transaction(async (txn: Transaction) => {
+    await verifyAttachments()
+
+    try {
+      tx = txn
+      await fetchExistingDocs()
+      await websqlProcessDocs()
+      sqliteChanges.notify(api._name)
+    } catch (err: any) {
+      throw handleSQLiteError(err)
     }
   })
+
+  return results
 }
+
+export default sqliteBulkDocs

src/constants.ts
@@ -1,8 +1,29 @@
-export const ADAPTER_VERSION = 1
+function quote(str: string) {
+  return "'" + str + "'"
+}
 
-export const DOC_STORE = '"document-store"'
-export const BY_SEQ_STORE = '"by-sequence"'
-export const ATTACH_STORE = '"attach-store"'
-export const LOCAL_STORE = '"local-docs"'
-export const META_STORE = '"metadata"'
-export const ATTACH_AND_SEQ_STORE = '"attach-seq"'
+const ADAPTER_VERSION = 7 // used to manage migrations
+
+// The object stores created for each database
+// DOC_STORE stores the document meta data, its revision history and state
+const DOC_STORE = quote('document-store')
+// BY_SEQ_STORE stores a particular version of a document, keyed by its
+// sequence id
+const BY_SEQ_STORE = quote('by-sequence')
+// Where we store attachments
+const ATTACH_STORE = quote('attach-store')
+const LOCAL_STORE = quote('local-store')
+const META_STORE = quote('metadata-store')
+// where we store many-to-many relations between attachment
+// digests and seqs
+const ATTACH_AND_SEQ_STORE = quote('attach-seq-store')
+
+export {
+  ADAPTER_VERSION as ADAPTER_VERSION,
+  DOC_STORE as DOC_STORE,
+  BY_SEQ_STORE as BY_SEQ_STORE,
+  ATTACH_STORE as ATTACH_STORE,
+  LOCAL_STORE as LOCAL_STORE,
+  META_STORE as META_STORE,
+  ATTACH_AND_SEQ_STORE as ATTACH_AND_SEQ_STORE,
+}

src/debug.ts
@@ -1,0 +1,9 @@
+import debug from 'debug'
+
+export const logger = {
+  debug: debug('pouch-sqlite:debug'),
+  error: debug('pouch-sqlite:error'),
+}
+
+logger.debug.log = console.log.bind(console)
+logger.error.log = console.error.bind(console)

src/index.ts
@@ -1,6 +1,10 @@
 import SqlPouchCore from './core'
+import type { OpenDatabaseOptions } from './openDatabase'
 
-function SQLitePouch(opts: any, callback: (err: any) => void) {
+function ReactNativeSQLitePouch(
+  opts: OpenDatabaseOptions,
+  callback: (err: any) => void
+) {
   try {
     // @ts-ignore
     SqlPouchCore.call(this, opts, callback)
@@ -9,11 +13,12 @@ function SQLitePouch(opts: any, callback: (err: any) => void) {
   }
 }
 
-SQLitePouch.valid = function () {
+// Set static properties
+ReactNativeSQLitePouch.valid = function () {
   return true
 }
-SQLitePouch.use_prefix = false
+ReactNativeSQLitePouch.use_prefix = false
 
-export default function sqlitePlugin(PouchDB: any) {
-  PouchDB.adapter('sqlite3', SQLitePouch, true)
+export default function reactNativeSqlitePlugin(PouchDB: any) {
+  PouchDB.adapter('react-native-sqlite', ReactNativeSQLitePouch, true)
 }

src/openDatabase.ts
@@ -1,0 +1,51 @@
+import { open } from '@op-engineering/op-sqlite'
+import { TransactionQueue } from './transactionQueue'
+import type { DB } from '@op-engineering/op-sqlite'
+
+type SQLiteOpenParams = Parameters<typeof open>
+export type OpenDatabaseOptions = SQLiteOpenParams[0] & {
+  revs_limit?: number
+}
+type OpenDatabaseResult =
+  | {
+      db: DB
+      transactionQueue: TransactionQueue
+    }
+  | {
+      error: Error
+    }
+
+const cachedDatabases = new Map<string, OpenDatabaseResult>()
+
+function openDBSafely(opts: OpenDatabaseOptions): OpenDatabaseResult {
+  try {
+    const db = open(opts)
+    const transactionQueue = new TransactionQueue(db)
+    return { db, transactionQueue }
+  } catch (err: any) {
+    return { error: err }
+  }
+}
+
+function openDB(opts: OpenDatabaseOptions) {
+  let cachedResult: OpenDatabaseResult | undefined = cachedDatabases.get(
+    opts.name
+  )
+  if (!cachedResult) {
+    cachedResult = openDBSafely(opts)
+    cachedDatabases.set(opts.name, cachedResult)
+  }
+  return cachedResult
+}
+
+export function closeDB(name: string) {
+  const cachedResult = cachedDatabases.get(name)
+  if (cachedResult) {
+    if ('db' in cachedResult) {
+      cachedResult.db.close()
+    }
+    cachedDatabases.delete(name)
+  }
+}
+
+export default openDB

commit 80c3b0248498c37339d16235670b51e3b744764d

fix(compaction): reclaim orphaned attachments
compactRevs never invoked deleteOrphans() on the success path: the checkDone() counter that triggers it was only reached via the "already deleted" branch (a `return checkDone()` that exited the whole function). Compaction therefore left attachment rows orphaned in the attach store, and a later put with a stub referencing a since-compacted attachment was accepted instead of returning 412.

Also fixes an early `return` in the orphan loop that skipped the remaining digests once one was still referenced (now `continue`).

Resolves the #8456 conformance test. Regression from 631d40d.

src/utils.ts
@@ -63,15 +63,7 @@ async function compactRevs(
     return
   }
 
-  let numDone = 0
   const seqs: number[] = []
-
-  function checkDone() {
-    if (++numDone === revs.length) {
-      // done
-      deleteOrphans()
-    }
-  }
 
   async function deleteOrphans() {
     // find orphaned attachment digests
@@ -119,7 +111,7 @@ async function compactRevs(
     }
     for (const digest of digestsToCheck) {
       if (nonOrphanedDigests.has(digest)) {
-        return
+        continue
       }
       await tx.execute(
         'DELETE FROM ' + ATTACH_AND_SEQ_STORE + ' WHERE digest=?',
@@ -131,20 +123,23 @@ async function compactRevs(
     }
   }
 
-  // update by-seq and attach stores in parallel
+  // collect the seqs for the revs being compacted and drop their by-seq rows
   for (const rev of revs) {
     const sql = 'SELECT seq FROM ' + BY_SEQ_STORE + ' WHERE doc_id=? AND rev=?'
 
     const res = await tx.execute(sql, [docId, rev])
     if (!res.rows?.length) {
       // already deleted
-      return checkDone()
+      continue
     }
     const seq = res.rows[0]!.seq as number
     seqs.push(seq)
 
     await tx.execute('DELETE FROM ' + BY_SEQ_STORE + ' WHERE seq=?', [seq])
   }
+
+  // now that all by-seq rows are gone, clean up any orphaned attachments
+  await deleteOrphans()
 }
 
 export function handleSQLiteError(

tests/attachments.test.js
@@ -4966,8 +4966,7 @@ repl_adapters.forEach(function (adapters) {
         }
       })
 
-      // Cannot resolve this case for me 😂
-      it.skip('#8456 bad attachment rev after replication', async function () {
+      it('#8456 bad attachment rev after replication', async function () {
         const db = new PouchDB(dbs.name, {})
         const remote = new PouchDB(dbs.remote, {})
 